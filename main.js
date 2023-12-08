73299f977b870067c7ad760976242396da17667a
grarere <grarere7@gmail.com>
2023-12-07 17:52:44 -0700

    css
    css

--- main.js
+++ main.js
@@ -3,3 +3,15 @@
 </p>
 const d = new Date("2023-12-6");
 </footer>
+function onButtonClick() {
+    alert('Button clicked!');
+  }
+  
+  const button = document.querySelector('button');
+  button.addEventListener('click', onButtonClick);
+constnewButton = document.createElement('btn-alert');
+newButton.textContent= 'Drums';
+document.body.appendChild(newButton);
+
+newButton.addEventListener('click',() => {alert('New button clicked!')});
+
